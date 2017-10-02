c7b23fcf8e3f41687e9f605cb991d587514d6ea6
Gao Kevin <kgao@lcs-mba3111.lcs.on.ca>
2017-10-02 10:56:14 -0400

    Get the eyes done

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -16,23 +16,46 @@
  Have fun!
  
  */
-// These are some required statements to make this playground work.
+// These are some required statements to make this playground.
 import Cocoa
 import PlaygroundSupport
 
+
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let c = Canvas(width: 500, height: 500)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
 
-// Show where the origin is
-canvas.drawAxes()
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+
+//eyes
+c.fillColor
+c.rotate(by: 45)
+c.translate(byX: 300, byY: -100)
+c.drawEllipse(centreX: 200, centreY: 30, width: 70, height: 130, borderWidth: 50)
+c.rotate(by: -85)
+c.drawEllipse(centreX: -190, centreY: 50, width: 70, height: 130, borderWidth: 50)
+
+//fill colour of eyes
+c.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100)
+c.drawEllipse(centreX: -185, centreY: 90, width: 30, height: 30)
+
+c.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100)
+c.drawEllipse(centreX: -55, centreY: 200, width: 30, height: 30)
+
+//draw nose
+c.rotate(by: 40)
+c.fillColor = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
+c.drawEllipse(centreX: 3, centreY: 80, width: 80, height: 40)
+
+// 
+c.fillColor = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
+
+
+//axes
+c.drawAxes()
+
+//
 
 
 // This code is necessary to see the result in the Assistant Editor at right
-PlaygroundPage.current.liveView = canvas.imageView
+PlaygroundPage.current.liveView = c.imageView
